65a7b49277beddd0d20ba9bbb5baf5e519d6d834
Jean-Sebastien BEVILACQUA <realitix@gmail.com>
2019-11-04 18:27:38 +0100

    Remove sessionauthentication

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         
     ],
     'DEFAULT_PERMISSION_CLASSES': [
